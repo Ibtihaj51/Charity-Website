f60233d1352f6b605d0ff7eb28ecf32e88ab3ea8
unknown <ibtihaj.ali51@gmail.com>
2022-09-21 01:21:35 -0700

    Charity Website

--- js/google-map.js
+++ js/google-map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(24.8607, 67.0011);
     // 39.399872
     // -8.224454
     
@@ -43,7 +43,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['New York'];
+    var addresses = ['Karachi'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
